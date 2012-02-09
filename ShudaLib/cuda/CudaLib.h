c360a42e53df4de165a229b3c097e3c0fdcfc407
lishuda1980 <lishuda1980@gmail.com>
2012-02-09 08:07:12 +0000

    using cv::gpu::GpuMat for cuda codes add gpuAlignDepthWithRGB() and gpuUnProjectIR()

--- ShudaLib/cuda/CudaLib.h
+++ ShudaLib/cuda/CudaLib.h
@@ -6,8 +6,12 @@ namespace btl
 namespace cuda_util
 {
 
-int cudaDepth2Disparity( const float* pDepth_, const int& nRow_, const int& nCol_, float *pDisparity_  ); 
-int cudaDisparity2Depth( const float* pDisparity_, const int& nRow_, const int& nCol_, float *pDepth_  );
+void cudaDepth2Disparity( const cv::gpu::GpuMat& cvgmDepth_, cv::gpu::GpuMat* pcvgmDisparity_ );
+void cudaDisparity2Depth( const cv::gpu::GpuMat& cvgmDisparity_, cv::gpu::GpuMat* pcvgmDepth_ );
+
+void cudaUnProjectIR(const cv::gpu::GpuMat& cvgmUndistortDepth_ , 
+	const double& dFxIR_, const double& dFyIR_, const double& uIR_, const double& vIR_, 
+	cv::gpu::GpuMat* pcvgmIRWorld_ );
 
 }//cuda_util
 }//btl
